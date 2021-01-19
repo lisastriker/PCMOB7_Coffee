03321bef75685d90fce80086729d7cf790f4103b
lisastriker <lisastriker@gmail.com>
2021-01-19 15:49:02 +0800

    added sqlit but no working yet

--- screens/create.js
+++ screens/create.js
@@ -1,29 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import  {NavigationContainer} from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
 import {Rating, AirbnbRating} from 'react-native-ratings'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+
 
 const COFFEE_IMAGE = require('../assets/coffee_bean.png')
 export default function createScreen({navigation}){
     const [rate, setRate] = useState()
     const [store, setStore] = useState("")
     const [coffeeBean, setCoffeeBean] = useState("")
+    const [flavor, setFlavor] = useState("")
+
     return(
         <View style={styles.container}> 
             <Text>Create Screen</Text>
             <TextInput 
             style={styles.input}
-            placeholder="Shop name" 
+            placeholder="Shop name"
+            value={store} 
             onChangeText={text=>setStore(text)}
             />     
             <TextInput 
             style={styles.input} 
             placeholder="Coffee Bean Type" 
+            value={coffeeBean}
             onChangeText={text=>setCoffeeBean(text)}
-            />             
+            />
+            <TextInput 
+            style={styles.input} 
+            placeholder="Flavor/Taste" 
+            value={flavor}
+            onChangeText={text=>setFlavor(text)}
+            />              
             <Text>{store}</Text>
             <Text>{coffeeBean}</Text>
             <Text>Rate the coffee</Text> 
@@ -37,7 +49,11 @@ export default function createScreen({navigation}){
             onFinishRating={setRate}
             style={{ paddingVertical: 10 }}
             />
-            <Text>{rate}</Text>      
+            <Text>{rate}</Text>
+            <TouchableOpacity style={styles.button} onPress={()=>navigation.navigate('index', {
+                rate, store, coffeeBean, flavor})}>
+            <Text>Submit</Text>
+            </TouchableOpacity> 
         </View>
     )
 
@@ -54,5 +70,11 @@ const styles = StyleSheet.create({
      height: 40, 
      borderColor: 'gray', 
      borderWidth: 1,
+    },
+    button:{
+        width:100,
+        height:30,
+        borderColor:"grey",
+        color:"black"
     }
   });
--- screens/index.js
+++ screens/index.js
@@ -1,24 +1,112 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 import  {NavigationContainer} from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import * as SQLite from 'expo-sqlite';
 
-export default function indexScreen({navigation}){
 
-    useEffect(()=>{
+const db = SQLite.openDatabase("db.db");
+
+export default function indexScreen({navigation, route}){
+    const [arrayCoffee, setArrayCoffee] = useState([])
+    function refreshInfo() {
+        console.log("inside refreshInfo")
+        db.transaction((tx) => {
+          tx.executeSql(
+            "SELECT * FROM coffee",
+            null,
+            (txObj, { rows: { _array } }) => {console.log(_array), setArrayCoffee(_array)},
+            (txObj, error) => console.log(`Error: ${error}`)
+          );
+        });
+      }
+      function deleteDatabase(){
+        db.transaction((tx) => {
+          tx.executeSql(
+            "DROP TABLE IF EXISTS notes",
+            null,
+            refreshNotes,
+            (txObj, error) => console.log(`Error: ${error}`)
+          );
+        }, console.log("didn't delete"), console.log("deleted database"));
+      }
+
+    useEffect(() => { //Create Database
+        db.transaction(tx => {
+          tx.executeSql(
+            `CREATE TABLE IF NOT EXISTS coffee
+            (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                store_name TEXT, rating REAL, coffee_bean TEXT, flavor TEXT);`
+          );
+        }, null, console.log("database created"));
+      }, []);
+
+    useEffect(()=>{ //Insert into database
+        console.log("Watching params")
+        if(route.params?.store){
+            console.log(route.params.store)
+            console.log(route.params.coffeeBean)
+            console.log(route.params.flavor)
+            db.transaction(tx=>{
+                tx.executeSql('INSERT into coffee (store_name, coffee_bean, flavor) VALUES (?,?,?),' 
+                [route.params.store, route.params.coffeeBean, route.params.flavor]);
+            }, console.log("database no insert"), refreshInfo)
+        } else {
+            console.log("Insert not happening")
+        }
+    }, [route.params?.store])
+
+    
+    useEffect(()=>{ //Create header
         navigation.setOptions ({
             headerRight:()=>(
             <Button title="create" onPress={()=>navigation.navigate("create")}>
             </Button>
-        )})
+        ),
+        headerLeft:()=>(
+            <Button title="delete" onPress={()=>deleteDatabase}></Button>
+        )
+        })
     },[])
     
+    // function renderItem({ item }) {
+    //     return (
+    //       <View
+    //         style={{
+    //           padding: 10,
+    //           paddingTop: 20,
+    //           paddingBottom: 20,
+    //           borderBottomColor: "#ccc",
+    //           borderBottomWidth: 1,
+    //         }}
+    //       >
+    //         <Text>{item.store_name}</Text>
+    //         <Text>{item.rate}</Text>
+    //       </View>
+    //     );
+    //   }
+
     return(
-        <View>
+        <View style={styles.container}>
             <Text>Index Screen</Text>
-        </View>
+        {/* <FlatList
+          data={arrayCoffee}
+          renderItem={renderItem}
+          style={{ width: "100%" }}
+          keyExtractor={(item) => item.id.toString()}
+        /> */}
+      </View>
     )
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+});
